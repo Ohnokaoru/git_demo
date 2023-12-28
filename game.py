8bc655e411a3ef08e9e205c0a09dc46460721333
kaoru <kaoru@gmail.com>
2023-12-28 18:32:05 +0800

    完成不需要的檔案刪除

---
+++ game.py
@@ -0,0 +1,16 @@
+import random
+
+
+x=random.randint(1,50)
+print(x)
+for i in range(5):
+    y=eval(input("請輸入數字(1-50:)"))
+    if y==x:
+        print("猜對了")
+        break
+    else:
+        print("猜錯了")
+
+
+   
+
